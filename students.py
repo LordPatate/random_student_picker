903cf58c7972ddd2f81555577d1d5cf402de137f
LordPatate <tom.bricaud1@orange.fr>
2026-09-22 13:47:50 +0200

    Change CSV reader to safer DictReader

--- students.py
+++ students.py
@@ -7,10 +7,21 @@ class Student(NamedTuple):
     first_name: str
     last_name: str
     email: str
-    uuid: str
+    uid: str
+
+
+def snake_case(name: str) -> str:
+    return name.strip().lower().replace(" ", "_")
 
 
 def students_from_file(csv_file_name: str) -> list[Student]:
     with open(csv_file_name, newline="") as csv_file:
-        reader = csv.reader(csv_file)
-        return [Student._make(row[:len(Student._fields)]) for row in reader]
+        reader = csv.DictReader(csv_file)
+        return [
+            Student(**{
+                snake_case(field_name): value
+                for field_name, value in row.items()
+                if field_name.strip() != ""
+            })
+            for row in reader
+        ]
